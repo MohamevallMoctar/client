fca8f5028a4767791bbd21930173e64d8bcf0d38
MohamevallMoctar <medvalloper@hotmail.com>
2025-01-03 21:11:42 +0000

    add ngx-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  http = inject(HttpClient)
+  title = 'Dating app';
+  users : any;
+
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe({
+      next: (res) => {
+        this.users = res
+        console.log(this.users[0])
+      },
+      error: (error) => {console.log(error)},
+      complete: () => {console.log('completed')}
+    })
+  }
 }
